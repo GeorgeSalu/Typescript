81c7419cbb7212f26a9cf5ae139def0b28694739
George Salu <george.salu10@gmail.com>
2020-06-24 16:54:25 -0300

    enums

---
+++ tipos/enums.ts
@@ -0,0 +1,12 @@
+//enums
+enum Cor {
+  Cinza,
+  Verde = 100,
+  Azul,
+  Vermelho = 100
+}
+
+let minhaCor: Cor = Cor.Verde
+console.log(minhaCor)
+console.log(Cor.Azul)
+console.log(Cor.Vermelho)
