8f39d956b0bd6d5b05f68d24b2025647b25c1b55
George Salu <george.salu10@gmail.com>
2020-06-24 21:14:20 -0300

    desafio objetos

---
+++ tipos/objetosDesafio.ts
@@ -0,0 +1,7 @@
+/**
+ * criar um objeto funcionario com:
+ *  - Array de strings com os nomes dos supervisores
+ *  - função de bater ponto que recebe a hora (numero) e retorna uma string
+ *    -> ponto nomrmal (<= 18)
+ *    -> fora do horario (> 8)
+ */
