e722504ff7080fbf207c072af160a44fedd31487
George Salu <george.salu10@gmail.com>
2020-06-25 10:18:49 -0300

    strictNullChecks, noUnusedParameters e noUnusedLocals

---
+++ compilador/compilador3.ts
@@ -0,0 +1,15 @@
+
+function saudar(isManha: boolean): string {
+  let saudacao: string 
+  if(isManha) {
+    saudacao = 'Bom dia'
+  } else {
+    saudacao = 'Tenha uma boa vida'
+  }
+  return saudacao
+}
+
+/**
+ * strictNullChecks, noUnusedParameters e noUnusedLocals
+ * 
+ */
