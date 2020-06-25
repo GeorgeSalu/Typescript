08bda6f2d70f09066c6498e3a7762b9692b25709
George Salu <george.salu10@gmail.com>
2020-06-25 14:14:41 -0300

    template string

---
+++ ecmascript/templateString.ts
@@ -0,0 +1,8 @@
+const usuarioID: string = 'suporte coder'
+const notificacoes: string = '9'
+const boasVindas = 'Boas vindas '+usuarioID+' Notificacoes '+notificacoes
+
+const boasVindas2 = `Boas vindas ${usuarioID} Notificações ${notificacoes}`
+
+console.log(boasVindas)
+console.log(boasVindas2)
