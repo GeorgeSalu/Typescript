5a50dfb262f85f605af47d692444be6ccd659027
George Salu <george.salu10@gmail.com>
2020-06-25 09:04:29 -0300

    definindo tipos personalizados

---
+++ tipos/tiposPersonalizados.ts
@@ -0,0 +1,26 @@
+type Funcionario = {
+  supervisores: string[],
+  baterPonto: (horas: number) => string
+}
+
+let funcionario1: Funcionario = {
+  supervisores: ['Ana', 'Fernando'],
+  baterPonto(horario: number): string {
+    if(horario <= 8) {
+      return 'Ponto normal'
+    } else {
+      return 'Fora do horario'
+    }
+  }
+}
+
+let funcionario2: Funcionario = {
+  supervisores: ['Ana', 'Fernando'],
+  baterPonto(horario: number): string {
+    if(horario <= 8) {
+      return 'Ponto normal'
+    } else {
+      return 'Fora do horario'
+    }
+  }
+}
