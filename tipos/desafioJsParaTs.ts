5651634aa6eca0b755788ad9acb174b83784f21e
George Salu <george.salu10@gmail.com>
2020-06-25 09:29:53 -0300

    desafio transformar codigo js em ts - resposta

--- tipos/desafioJsParaTs.ts
+++ tipos/desafioJsParaTs.ts
@@ -1,12 +1,23 @@
 //desafio
-let contaBancaria = {
+type ContaBancaria = {
+  saldo: number,
+  depositar: (valor: number) => void
+}
+
+let contaBancaria: ContaBancaria = {
   saldo: 3445,
-  depositar(valor) {
+  depositar(valor: number) {
     this.saldo += valor
   }
 }
 
-let correntista = {
+type Correntista = {
+  nome: string,
+  contaBancaria: ContaBancaria,
+  contatos: string[]
+}
+
+let correntista: Correntista = {
   nome: 'ana silva',
   contaBancaria: contaBancaria,
   contatos: ['233434232','22323232']
