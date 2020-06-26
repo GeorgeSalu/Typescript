981acd68741b1be8e513b078bba24db40132b705
George Salu <george.salu10@gmail.com>
2020-06-26 11:39:36 -0300

    desafio classe - resposta

--- classes/desafioClasse.ts
+++ classes/desafioClasse.ts
@@ -5,3 +5,17 @@
  * obs 1 : desconto é opcional (valor padrao 0)
  * obs 2 : criar dois produtos: passando dois e tres params
  */
+
+ class Produto {
+   constructor(public nome: string, public preco: number,
+      public desconto: number = 0) {
+
+      }
+ }
+
+ const prod1 = new Produto('caneta bic preta', 4.20)
+ prod1.desconto = 0.06
+ console.log(prod1)
+
+ const prod2 = new Produto('Caderno escolar', 18.00, 0.15)
+ console.log(prod2)
