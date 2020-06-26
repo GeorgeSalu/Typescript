7a1ffbd401069d274be0e4716bb5b61e7f3f102f
George Salu <george.salu10@gmail.com>
2020-06-26 16:49:38 -0300

    desafio 02

---
+++ classes/desafio02.ts
@@ -0,0 +1,13 @@
+// Exercício 2 - Herança
+var objeto2D = {
+  base: 0,
+  altura: 0
+}
+
+var retangulo = Object.create(objeto2D)
+retangulo.base = 5
+retangulo.altura = 7
+retangulo.area = function() {
+  return this.base * this.altura
+}
+console.log(retangulo.area())
