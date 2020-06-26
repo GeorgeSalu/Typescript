1e14cd49c575eab3fb71bfb7a60047418ae3a0e2
George Salu <george.salu10@gmail.com>
2020-06-26 10:41:48 -0300

    classes e atributos #02

---
+++ classes/classesEAtributos02.ts
@@ -0,0 +1,14 @@
+class DataEsperta {
+
+  constructor(public dia: number = 1, public mes: number = 1,public ano: number = 1970) { }
+
+}
+
+const aniversarioEsperto = new DataEsperta(3, 11, 1991)
+aniversarioEsperto.dia = 4
+console.log(aniversarioEsperto.dia)
+console.log(aniversarioEsperto)
+
+const casamentoEsperto = new DataEsperta // posso omitir os "()"
+casamentoEsperto.ano = 2017
+console.log(casamentoEsperto)
