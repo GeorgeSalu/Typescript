16fddaa0d119d06d9c2140af388427a58b20ac09
George Salu <george.salu10@gmail.com>
2020-06-26 12:27:15 -0300

    desafio metodo - indentando codigo

--- classes/desafioClasse.ts
+++ classes/desafioClasse.ts
@@ -6,29 +6,29 @@
  * obs 2 : criar dois produtos: passando dois e tres params
  */
 
- class Produto {
-   constructor(public nome: string, public preco: number,
-      public desconto: number = 0) {}
+class Produto {
+  constructor(public nome: string, public preco: number,
+    public desconto: number = 0) {}
 
-    /**
-     * criar um metodo precoComDesconto
-     * quais são os paramentros e o retorno ?
-     * alterar método resumo para mostrar o preço com desconto
-     */
-    precoComDesconto(): number {
-      return this.preco * (1 - this.desconto)
-    }
+  /**
+   * criar um metodo precoComDesconto
+   * quais são os paramentros e o retorno ?
+   * alterar método resumo para mostrar o preço com desconto
+   */
+  precoComDesconto(): number {
+    return this.preco * (1 - this.desconto)
+  }
 
-    public resumo(): string {
-      return `${this.nome} custa R$ ${this.precoComDesconto()} (${this.desconto * 100}% off)`
-    }
- }
+  public resumo(): string {
+    return `${this.nome} custa R$ ${this.precoComDesconto()} (${this.desconto * 100}% off)`
+  }
+}
 
- const prod1 = new Produto('caneta bic preta', 4.20)
- prod1.desconto = 0.06
- console.log(prod1)
- console.log(prod1.resumo())
+const prod1 = new Produto('caneta bic preta', 4.20)
+prod1.desconto = 0.06
+console.log(prod1)
+console.log(prod1.resumo())
 
- const prod2 = new Produto('Caderno escolar', 18.00, 0.15)
- console.log(prod2)
- console.log(prod2.resumo())
+const prod2 = new Produto('Caderno escolar', 18.00, 0.15)
+console.log(prod2)
+console.log(prod2.resumo())
