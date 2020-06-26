a1cdb05952fdd624ac62fc544ba7b62cfea50d59
George Salu <george.salu10@gmail.com>
2020-06-26 10:44:36 -0300

    desafio classe

---
+++ classes/desafioClasse.ts
@@ -0,0 +1,7 @@
+/**
+ * Desafio Classe Produto
+ * Atributos: nome, preco e desconto
+ * criar construtor
+ * obs 1 : desconto é opcional (valor padrao 0)
+ * obs 2 : criar dois produtos: passando dois e tres params
+ */
