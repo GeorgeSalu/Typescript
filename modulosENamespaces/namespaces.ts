045606bedb6a599f9b1818a2fa42fc5d9db5d993
George Salu <george.salu10@gmail.com>
2020-06-27 19:45:52 -0300

    namespaces aninhados

--- modulosENamespaces/namespaces.ts
+++ modulosENamespaces/namespaces.ts
@@ -1,17 +1,19 @@
-namespace Areas {
+namespace Geometria {
+  export namespace Areas {
 
-  const PI = 3.14
-  
-  export function circunferencia(raio: number): number {
-    return PI * Math.pow(raio, 2)
+    const PI = 3.14
+    
+    export function circunferencia(raio: number): number {
+      return PI * Math.pow(raio, 2)
+    }
+    
+    export function retangulo(base: number, altura: number): number {
+      return base * altura
+    }
+    
   }
-  
-  export function retangulo(base: number, altura: number): number {
-    return base * altura
-  }
-  
 }
 
-console.log(Areas.circunferencia(10))
-console.log(Areas.retangulo(10,20))
+console.log(Geometria.Areas.circunferencia(10))
+console.log(Geometria.Areas.retangulo(10,20))
 
