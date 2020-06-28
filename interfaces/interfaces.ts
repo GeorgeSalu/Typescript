8e8987ac151778460ffc0b335e1b1abb43047bfd
George Salu <george.salu10@gmail.com>
2020-06-28 12:21:21 -0300

    uso de interface para estender Object

--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -97,3 +97,25 @@ function teste(a: B) {
 }
 
 teste(new RealABC)
+
+// interface para estender Object
+
+interface Object {
+  log(): void
+}
+
+Object.prototype.log = function() {
+  console.log(this.toString())
+}
+
+const x = 2
+const y = 3
+const z = 4
+
+console.log(x)
+console.log(y)
+console.log(z)
+
+x.log()
+y.log()
+z.log()
