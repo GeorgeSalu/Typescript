f29e4272188145cfc721f3a60f9b4577b6874116
George Salu <george.salu10@gmail.com>
2020-06-27 23:15:46 -0300

    o basico de interface

--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -1,8 +1,12 @@
-function saudarComOla(pessoa: { nome: string }) {
+interface Humano {
+  nome: string
+}
+
+function saudarComOla(pessoa: Humano) {
   console.log('Ola, '+pessoa.nome)
 }
 
-function mudarNome(pessoa: { nome: string }) {
+function mudarNome(pessoa: Humano) {
   pessoa.nome = 'joana'
 }
 
