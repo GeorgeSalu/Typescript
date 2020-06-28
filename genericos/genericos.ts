822f4f58b47ab648689545d301080b8d1f06ba3c
George Salu <george.salu10@gmail.com>
2020-06-28 12:38:04 -0300

    criando uma funcao com generics

--- genericos/genericos.ts
+++ genericos/genericos.ts
@@ -5,3 +5,12 @@ function echo (objeto: any) {
 console.log(echo('joao'))
 console.log(echo(27))
 console.log(echo({ nome: 'joao', idade: 23 }))
+
+//usando generics
+function echoMelhorado<T>(objeto: T): T {
+  return objeto
+}
+
+console.log(echoMelhorado('joao'))
+console.log(echoMelhorado<number>(27))
+console.log(echoMelhorado({ nome: 'joao', idade: 23 }))
