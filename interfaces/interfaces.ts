3d8ff414ff4f667698147a99fc25562b6d332a3e
George Salu <george.salu10@gmail.com>
2020-06-28 11:52:34 -0300

    heranca com interface

--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -57,3 +57,43 @@ potencia = function(base: number, exp: number) : number {
 }
 
 console.log(potencia(3, 10))
+
+// Heranca
+interface A {
+  a(): void
+}
+
+interface B {
+  b(): void
+}
+
+interface ABC extends A, B {
+  c(): void
+}
+
+class RealA implements A {
+  a(): void {}
+}
+
+class RealAB implements A, B {
+  a(): void {}
+  b(): void {}
+}
+
+class RealABC implements ABC {
+  a(): void {}
+  b(): void {}
+  c(): void {}
+}
+
+abstract class AbstractABD implements A, B {
+  a(): void {}
+  b(): void {}
+  abstract d(): void
+}
+
+function teste(a: B) {
+
+}
+
+teste(new RealABC)
