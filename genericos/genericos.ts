bd920a6eb86f57cf54bf069355b926621be48651
George Salu <george.salu10@gmail.com>
2020-06-28 15:06:12 -0300

    criando classes com generics #03

--- genericos/genericos.ts
+++ genericos/genericos.ts
@@ -66,3 +66,22 @@ class SomaBinaria extends OperacaoBinaria<number, number> {
 
 console.log(new SomaBinaria(3,4).executar())
 console.log(new SomaBinaria(3,422).executar())
+
+class DiferencaEntreDatas extends OperacaoBinaria<Data, string> {
+  getTime(data: Data): number {
+    let { dia, mes, ano } = data
+    return new Date(`${mes}/${dia}/${ano}`).getTime()
+  }
+
+  executar(): string {
+    const t1 = this.getTime(this.operando1)
+    const t2 = this.getTime(this.operando2)
+    const diferenca = Math.abs(t1 - t2)
+    const dia = 1000 * 60 * 60 * 24
+    return `${Math.ceil(diferenca / dia)} dia(s)`
+  }
+}
+
+const d1 = new Data(1, 2, 2030)
+const d2 = new Data(1, 3, 2030)
+console.log(new DiferencaEntreDatas(d1, d2).executar())
