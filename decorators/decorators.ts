afd8059f89b88d9cdcaaa4bb14680da74517462f
George Salu <george.salu10@gmail.com>
2020-06-28 21:07:37 -0300

    decorator factory

--- decorators/decorators.ts
+++ decorators/decorators.ts
@@ -1,4 +1,6 @@
-@logarClasse
+//@logarClasse
+//@logarClasseSe(true)
+@decorator('abc',111)
 class Eletrodomestico {
   constructor() {
     console.log('novo ....')
@@ -10,3 +12,13 @@ function logarClasse(construtor: Function) {
 }
 
 new Eletrodomestico()
+
+function logarClasseSe(valor: boolean) {
+  return valor ? logarClasse : null
+}
+
+function decorator(a: string, b: number) {
+  return function(contrutor: Function): void {
+    console.log(a + ''+ b)
+  }
+}
