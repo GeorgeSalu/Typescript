6a30716f903e0f7356652cfde0b8c03fd9462102
George Salu <george.salu10@gmail.com>
2020-06-28 21:43:04 -0300

    desafio decorator de classe

--- decorators/decorators.ts
+++ decorators/decorators.ts
@@ -51,3 +51,18 @@ function imprimivel(construtor: Function) {
 }
 
 new Eletrodomestico().imprimir()
+
+// Desafio Decorator perfilAdmin
+const usuarioLogado = {
+  nome: 'Guilherme Filho',
+  email: 'guigui@gmail.com',
+  admin: false
+}
+
+class MudancaAdministrativa {
+  critico() {
+      console.log('Algo crítico foi alterado!')
+  }
+}
+
+new MudancaAdministrativa().critico()
