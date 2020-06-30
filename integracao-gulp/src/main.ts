642011c1d182a8a4201c493fd95d26d2ff2b007d
George Salu <george.salu10@gmail.com>
2020-06-30 09:30:54 -0300

    usando uma lib javascript

--- integracao-gulp/src/main.ts
+++ integracao-gulp/src/main.ts
@@ -1,4 +1,7 @@
+import $ from 'jquery'
 import Livro from './modelo/livro'
 
 const livro = new Livro('Dom quixote', 109.00, 0.10)
 console.log(livro.precoComDesconto())
+
+$('body').append(`<h1>${livro.nome}</h1>`)
